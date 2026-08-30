efcbcbdf05a10c3f9d329d96bdb752e42aa9cf42
mega-daun <160328509+mega-daun@users.noreply.github.com>
2026-08-30 06:15:04 +0000

    Apply automatic linting fixes

--- app/Telegram/Menus/WeekdaySelectionMenu.php
+++ app/Telegram/Menus/WeekdaySelectionMenu.php
@@ -12,19 +12,19 @@ trait WeekdaySelectionMenu
     protected function makeMultipleWeekdaySelectionMenu(array $selectedWorkdays, string $prefix): InlineKeyboardMarkup
     {
         $payload = collect(range(1, 7))->map(
-            fn(int $weekday) => in_array($weekday, $selectedWorkdays)
-                ? ['label' => __('prompt.general.marked', ['item' => __('general.weekday.' . $weekday)]), 'data' => $prefix . '.remove.' . $weekday]
-                : ['label' => __('general.weekday.' . $weekday), 'data' => $prefix . '.add.' . $weekday]
+            fn (int $weekday) => in_array($weekday, $selectedWorkdays)
+                ? ['label' => __('prompt.general.marked', ['item' => __('general.weekday.'.$weekday)]), 'data' => $prefix.'.remove.'.$weekday]
+                : ['label' => __('general.weekday.'.$weekday), 'data' => $prefix.'.add.'.$weekday]
         );
-        $payload->add(['label' => __('prompt.general.done'), 'data' => $prefix . '.done.done']);
+        $payload->add(['label' => __('prompt.general.done'), 'data' => $prefix.'.done.done']);
         $payload = $payload->map(
-            fn(array $item) => new InlineKeyboardButton(
+            fn (array $item) => new InlineKeyboardButton(
                 text: $item['label'],
                 callback_data: $item['data']
             )
         );
         $menu = InlineKeyboardMarkup::make();
-        $payload->each(fn(InlineKeyboardButton $btn) => $menu->addRow($btn));
+        $payload->each(fn (InlineKeyboardButton $btn) => $menu->addRow($btn));
 
         return $menu;
     }
@@ -32,16 +32,16 @@ trait WeekdaySelectionMenu
     protected function makeWeekdaySelectionMenu(string $prefix, array $weekdays = [1, 2, 3, 4, 5, 6, 7]): InlineKeyboardMarkup
     {
         $payload = collect($weekdays)->map(
-            fn(int $weekday) => ['label' => __('general.weekday.' . $weekday), 'data' => $prefix . '.add.' . $weekday]
+            fn (int $weekday) => ['label' => __('general.weekday.'.$weekday), 'data' => $prefix.'.add.'.$weekday]
         );
         $payload = $payload->map(
-            fn(array $item) => new InlineKeyboardButton(
+            fn (array $item) => new InlineKeyboardButton(
                 text: $item['label'],
                 callback_data: $item['data']
             )
         );
         $menu = InlineKeyboardMarkup::make();
-        $payload->each(fn(InlineKeyboardButton $btn) => $menu->addRow($btn));
+        $payload->each(fn (InlineKeyboardButton $btn) => $menu->addRow($btn));
 
         return $menu;
     }
